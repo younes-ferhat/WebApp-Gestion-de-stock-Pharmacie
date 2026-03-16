8e6519a4b8de79ca78a0f273254f23a089555f43
Anais khelifa <anaiskhelifa2001@gmail.com>
2026-03-17 00:51:56 +0100

    chore: sync DatabaseSeeder avec branche camarade - ajout fournisseurs Sanofi & Biogaran

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\Produit;
 use App\Models\Lot;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Fournisseur;
 
 class DatabaseSeeder extends Seeder
 {
@@ -95,6 +96,16 @@ class DatabaseSeeder extends Seeder
                 'produit_id' => $amoxicilline->id, // Liaison automatique
             ]
         );
+        // Juste avant la création des produits
+            $sanofi = Fournisseur::updateOrCreate(
+    ['email' => 'contact@sanofi.fr'],
+    ['nom' => 'Sanofi Aventis', 'telephone' => '0144777777']
+);
+
+        $biogaran = Fournisseur::updateOrCreate(
+    ['email' => 'info@biogaran.fr'],
+    ['nom' => 'Biogaran Laboratoires', 'telephone' => '0155667788']
+);
 
         $this->command->info('Base de données initialisée dynamiquement avec succès !');
     }
